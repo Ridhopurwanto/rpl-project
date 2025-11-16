fa5ee63cf03bafd0b4cce79fe071b639dda7f954
kessyades <222313163@stis.ac.id>
2025-11-16 23:51:41 +0700

    coba excel

--- routes/web.php
+++ routes/web.php
@@ -213,7 +213,6 @@ Route::middleware('auth')->group(function () {
             ->name('laporan.unduh');
         Route::post('/laporan/download', [LaporanUnduhController::class, 'download'])
             ->name('laporan.download');
-});
     });
 
     // --- RUTE UNTUK BAU ---
