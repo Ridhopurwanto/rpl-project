659d6ba4cb0f948ac7f98c269fc3468fa049eb7e
kessyades <222313163@stis.ac.id>
2025-12-19 09:26:22 +0700

    logika barang di komandan

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -30,8 +30,11 @@ class BarangController extends Controller
         
         if ($tanggalTemuan) {
             $queryTemuan->where(function($q) use ($tanggalTemuan) {
-                $q->whereDate('waktu_lapor', $tanggalTemuan)
-                  ->orWhereDate('waktu_selesai', $tanggalTemuan);
+                $q->whereDate('waktu_lapor', '<=', $tanggalTemuan)
+                  ->where(function($sub) use ($tanggalTemuan) {
+                      $sub->whereNull('waktu_selesai')
+                          ->orWhereDate('waktu_selesai', '>=', $tanggalTemuan);
+                  });
             });
         }
         
@@ -53,8 +56,11 @@ class BarangController extends Controller
         
         if ($tanggalTitipan) {
             $queryTitipan->where(function($q) use ($tanggalTitipan) {
-                $q->whereDate('waktu_titip', $tanggalTitipan)
-                  ->orWhereDate('waktu_selesai', $tanggalTitipan);
+                $q->whereDate('waktu_titip', '<=', $tanggalTitipan)
+                  ->where(function($sub) use ($tanggalTitipan) {
+                      $sub->whereNull('waktu_selesai')
+                          ->orWhereDate('waktu_selesai', '>=', $tanggalTitipan);
+                  });
             });
         }
         
