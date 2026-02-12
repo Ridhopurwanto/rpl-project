78e7f7bdb81533bfb4430a9fb3bf8fe38fc61de2
Ridhopurwanto <ridhopurwanto29@gmail.com>
2026-02-12 16:14:03 +0800

    clear main branch

--- resources/js/app.js
+++
@@ -1,5 +0,0 @@
-import './bootstrap';
-import '../css/app.css';
-
-// (Biarkan sisa kode yang sudah ada di bawahnya, 
-//  biasanya ada 'import ./bootstrap';)
--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// Semua route akan render view yang sama (SPA)
-Route::get('/{any}', function () {
-    return view('welcome');
-})->where('any', '.*');
